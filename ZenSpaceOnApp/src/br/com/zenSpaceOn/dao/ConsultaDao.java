e04726ef7f1a7fa50e8185c8ed4efde995a096e4
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-10-21 19:42:03 -0300

    Criação das interfaces DAO

---
+++ ZenSpaceOnApp/src/br/com/zenSpaceOn/dao/ConsultaDao.java
@@ -0,0 +1,18 @@
+package br.com.zenSpaceOn.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.zenSpaceOn.to.ConsultaTO;
+
+public interface ConsultaDao {
+public void insert(ConsultaTO consulta) throws SQLException;
+	
+	public List<ConsultaTO> select() throws SQLException;
+	
+	public ConsultaTO select(Integer codigo) throws SQLException;
+	
+	public void update(ConsultaTO agendamento, Integer codigo) throws SQLException;
+	
+	public void delete(Integer codigo) throws SQLException;
+}
